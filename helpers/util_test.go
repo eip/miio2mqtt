467ffc31cbec6463e133876a4999c98fe7577fb1
eip <eip@users.noreply.github.com>
2021-02-03 15:53:11 +0200

    helpers/util refactoring

--- helpers/util_test.go
+++ helpers/util_test.go
@@ -9,28 +9,28 @@ import (
 func Test_FromHex(t *testing.T) {
 	tests := []struct {
 		name string
-		data string
+		arg  string
 		want []byte
 	}{
 		{
 			name: "Sample hex string",
-			data: "000102030405060708090A0B0C0D0E0F",
+			arg:  "000102030405060708090A0B0C0D0E0F",
 			want: []byte{0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f},
 		},
 		{
 			name: "Invalid hex string 1",
-			data: "000102030405060708090A0B0C0D0E0FF",
+			arg:  "000102030405060708090A0B0C0D0E0FF",
 			want: nil,
 		},
 		{
 			name: "Invalid hex string 2",
-			data: "_000102030405060708090A0B0C0D0E0F",
+			arg:  "_000102030405060708090A0B0C0D0E0F",
 			want: nil,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got := FromHex(tt.data)
+			got := FromHex(tt.arg)
 			AssertEqual(t, got, tt.want)
 		})
 	}
@@ -39,75 +39,78 @@ func Test_FromHex(t *testing.T) {
 func Test_IsPrintableASCII(t *testing.T) {
 	tests := []struct {
 		name string
-		data []byte
+		arg  []byte
 		want bool
 	}{
 		{
 			name: "Printable slice",
-			data: []byte{0x20, 0x21, 0x22, 0x23, 0x7c, 0x7d, 0x7e, 0x7f},
+			arg:  []byte{0x20, 0x21, 0x22, 0x23, 0x7c, 0x7d, 0x7e, 0x7f},
 			want: true,
 		},
 		{
 			name: "Non-printable slice 1",
-			data: []byte{0x20, 0x21, 0x22, 0x23, 0x1f, 0x7c, 0x7d, 0x7e, 0x7f},
+			arg:  []byte{0x20, 0x21, 0x22, 0x23, 0x1f, 0x7c, 0x7d, 0x7e, 0x7f},
 			want: false,
 		},
 		{
 			name: "Non-printable slice 2",
-			data: []byte{0x20, 0x21, 0x22, 0x23, 0x80, 0x7c, 0x7d, 0x7e, 0x7f},
+			arg:  []byte{0x20, 0x21, 0x22, 0x23, 0x80, 0x7c, 0x7d, 0x7e, 0x7f},
 			want: false,
 		},
 		{
 			name: "Empty slice",
-			data: []byte{},
+			arg:  []byte{},
 			want: false,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got := IsPrintableASCII(tt.data)
+			got := IsPrintableASCII(tt.arg)
 			AssertEqual(t, got, tt.want)
 		})
 	}
 }
 
 func TestTestLog_Write(t *testing.T) {
+	type spy struct {
+		message string
+	}
 	tests := []struct {
-		name        string
-		log         *TestLog
-		p           []byte
-		want        int
-		wantMessage string
-		err         error
+		name string
+		log  *TestLog
+		arg  []byte
+		want int
+		err  error
+		spy  spy
 	}{
 		{
-			name:        "nil data",
-			log:         &TestLog{Message: "foo"},
-			p:           nil,
-			want:        0,
-			wantMessage: "",
+			name: "nil data",
+			log:  &TestLog{Message: "foo"},
+			arg:  nil,
+			want: 0,
+			spy:  spy{message: ""},
 		},
 		{
-			name:        "Empty data",
-			log:         &TestLog{Message: "foo"},
-			p:           []byte(""),
-			want:        0,
-			wantMessage: "",
+			name: "Empty data",
+			log:  &TestLog{Message: "foo"},
+			arg:  []byte(""),
+			want: 0,
+			spy:  spy{message: ""},
 		},
 		{
-			name:        "Sample string",
-			log:         &TestLog{Message: "foo"},
-			p:           []byte("bar!"),
-			want:        4,
-			wantMessage: "bar!",
+			name: "Sample string",
+			log:  &TestLog{Message: "foo"},
+			arg:  []byte("bar!"),
+			want: 4,
+			spy:  spy{message: "bar!"},
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := tt.log.Write(tt.p)
+			got, err := tt.log.Write(tt.arg)
 			AssertError(t, err, tt.err)
 			AssertEqual(t, got, tt.want)
-			AssertEqual(t, tt.log.Message, tt.wantMessage)
+			AssertEqual(t, tt.log.Message, tt.spy.message)
 		})
 	}
 }
